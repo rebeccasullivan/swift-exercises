ee00d4b839c965630c00d2477b3e4928229a1b66
Rebecca <rhhsullivan@gmail.com>
2017-11-29 08:19:04 -0500

    Start gigasecond exercise

---
+++ gigasecond/Sources/Gigasecond.swift
@@ -0,0 +1,11 @@
+//Solution goes in Sources
+// Calculate the moment when someone has lived for 109 seconds.
+// A gigasecond is 10^9 (1,000,000,000) seconds.
+
+class Gigasecond {
+    let from: String
+    
+    init(from: String) {
+        self.from = from
+    }
+}
